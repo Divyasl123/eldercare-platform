ca3f42db8d7ba798c3adda7cda9feccfc31d2383
Divya <divyasl2332@gmail.com>
2026-03-01 14:51:18 +0530

    Fixed port for render

--- server/server.js
+++ server/server.js
@@ -80,8 +80,8 @@ app.use((err, req, res, next) => {
    START SERVER
 ================================= */
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://127.0.0.1:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
